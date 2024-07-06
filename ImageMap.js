f4753073094c309745cb8476ce34c76689b94f92
RahKay500 <rakayor@st.ug.edu.gh>
2024-07-06 20:51:25 +0000

    Modified

---
+++ ImageMap.js
@@ -0,0 +1,12 @@
+const imageMap = {
+    <Image source= require('../assets/dress1.png'),
+    'dress2.png': require('../assets/dress2.png'),
+    'dress3.png': require('../assets/dress3.png'),
+    'dress4.png': require('../assets/dress4.png'),
+    'dress5.png': require('../assets/dress5.png'),
+    'dress6.png': require('../assets/dress6.png'),
+    'dress7.png': require('../assets/dress7.png'),
+    // Add more mappings as necessary
+};
+
+export default imageMap;
